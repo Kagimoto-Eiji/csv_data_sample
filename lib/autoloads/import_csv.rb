d3767335914b4498870cf557287978451386b1f5
kagimotoeiji <thank.eiji1017@gmail.com>
2021-04-21 11:47:20 +0900

    import_csv.rbのコード修正

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -2,12 +2,18 @@ require "csv"
 
 class ImportCsv
   def self.import(path)
+    list = []
     CSV.foreach(path, headers: true) do |row|
-      User.create!(
-        name: row["name"]
-        age: row["age"]
-        address: row["address"]
-      )
+      list << row.to_h
     end
+    list
+  end
+
+  def self.user_data
+    list = import('db/csv_data/user_data.csv')
+
+    puts "インポート処理を開始"
+    User.create!(list)
+    puts "インポート完了！！"
   end
 end
